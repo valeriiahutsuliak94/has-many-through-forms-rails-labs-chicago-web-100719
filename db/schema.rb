af3ec4e1d4eed0eb8b026e79c03025a91a06f8ed
Valeria <valeria.gutsuliak@gmail.com>
2019-11-11 08:58:41 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -25,20 +25,24 @@ ActiveRecord::Schema.define(version: 20160119152016) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  # add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  # add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "post_categories", force: :cascade do |t|
     t.integer  "post_id"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_post_categories_on_category_id"
+    t.index ["post_id"], name: "index_post_categories_on_post_id"
   end
 
-  add_index "post_categories", ["category_id"], name: "index_post_categories_on_category_id"
-  add_index "post_categories", ["post_id"], name: "index_post_categories_on_post_id"
+  # add_index "post_categories", ["category_id"], name: "index_post_categories_on_category_id"
+  # add_index "post_categories", ["post_id"], name: "index_post_categories_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
